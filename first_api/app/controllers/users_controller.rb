e1eed7d4ac9c2e1bd0278b8df90838ce16169ec8
carveyh <carvey.hor@gmail.com>
2023-04-12 17:14:10 -0400

    Near complete Phase IV

--- first_api/app/controllers/users_controller.rb
+++ first_api/app/controllers/users_controller.rb
@@ -5,17 +5,38 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
-
-        user.save!
-        render json: user
+        user = User.new(user_params)
+        #Is it necessary to do equivalent of @chirp.author? We did not do here, it was fine.
+        if user.save
+            render json: user
+            #Q: How does this work Ruby wise, passing in @instance to the _url shortcut?
+            #e.g. redirect_to chirp_url(@chirp)
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+        
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
     
+    def update
+        user = User.update(user_params)
+        render json: user
+    end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
     
 
 end
