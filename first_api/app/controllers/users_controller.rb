e31b68069e300dc88d23387ebfc0e89bc2c7847c
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-04-12 16:19:19 -0400

    phase four half way through

--- first_api/app/controllers/users_controller.rb
+++ first_api/app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+
+        user.save!
+        render json: user
     end
 
     def show
         render json: params
     end
     
+
+    
+
 end
--- first_api/db/seeds.rb
+++ first_api/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create([{name: "Merve", email: "merve@merve.merve"},{name: "Carve", email: "carve@carve.carve"}])
