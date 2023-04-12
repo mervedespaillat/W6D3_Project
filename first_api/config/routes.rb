6c7c62199f06be8ee0ec65d405bce81522d5ebc7
carveyh <carvey.hor@gmail.com>
2023-04-12 14:25:55 -0400

    Complete phase 1

--- first_api/config/routes.rb
+++ first_api/config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#edit', as: 'edit_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 
 end
