52f8c3e4c3aeddcc54c585298e22598eeed9c3cf
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-04-13 16:30:20 -0400

    Artwork_shares_controller created

--- first_api/config/routes.rb
+++ first_api/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :create, :destroy, :show, :update]
   resources :artworks, only: [:index, :create, :destroy, :show, :update]
+  resources :artwork_shares, only: [:create, :destroy]
+  
   # Defines the root path route ("/")
   # root "articles#index"
 
